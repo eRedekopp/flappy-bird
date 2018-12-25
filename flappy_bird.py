98b923dda42a4e6758ad0af86cc79500b991920f
Eric Redekopp <eric.redekopp@mail.com>
2018-12-25 02:21:20 -0600

    Added BarPair and BarList classes. Not yet scrolling properly

--- flappy_bird.py
+++ flappy_bird.py
@@ -15,6 +15,7 @@ GRAVITY = 3.5
 JUMP_VELOCITY = -35
 MAX_Y_VELOCITY = 12
 FRAME_RATE = 60
+SCROLL_RATE = 30
 
 # colours
 WHITE  = 255, 255, 255
@@ -25,29 +26,79 @@ GREY   = 100, 100, 100
 GREEN  = 40, 190, 40
 
 # graphics
-BIRD_RADIUS = 15    # the radius of the circle representing the bird
-BIRD_COLOUR = YELLOW
-BAR_WIDTH   = 60
+BIRD_RADIUS    = 15     # the radius of the circle representing the bird
+BIRD_COLOUR    = YELLOW
+BAR_WIDTH      = 60
+BAR_GAP        = 120
+BAR_FREQUENCY  = 30
+BAR_COLOUR     = GREEN
+BG_COLOUR      = BLUE
 MAX_BAR_LENGTH = 400
 MIN_BAR_LENGTH = 150
-GROUND_HEIGHT = 40
-GROUND_LEVEL  = SCREEN_HEIGHT - GROUND_HEIGHT # the position of the top of the
-                                              # ground
+GROUND_HEIGHT  = 40
+GROUND_LEVEL   = SCREEN_HEIGHT - GROUND_HEIGHT # the position of the top of the
+                                               # ground
 BIRD_INITIAL_POSITION = bird_x, bird_y= 200, GROUND_LEVEL - BIRD_RADIUS
 
+################################### Classes ####################################
 
-################################## Functions ###################################
+class BarPair:
+    @staticmethod
+    def generate_bar_pair():
+        height = rand.randint(MIN_BAR_LENGTH, MAX_BAR_LENGTH)
+        surface = pygame.Surface((BAR_WIDTH, GROUND_LEVEL))
+        bottom_bar = pygame.Surface((BAR_WIDTH, GROUND_LEVEL - height))
+        top_bar = pygame.Surface((BAR_WIDTH, GROUND_LEVEL - height - BAR_GAP))
+        bottom_bar.fill(BAR_COLOUR)
+        top_bar.fill(BAR_COLOUR)
+        surface.fill(BG_COLOUR)
+        surface.blit(top_bar, (0, 0))
+        surface.blit(bottom_bar, (0, GROUND_LEVEL - height))
+        return surface
 
-"""
-Returns a green surface of width BAR_WIDTH and random height within length 
-restrictions
-"""
-def generate_bar():
-    height = rand.randint(MIN_BAR_LENGTH, MAX_BAR_LENGTH)
-    surface = pygame.Surface((BAR_WIDTH, height))
-    surface.fill(GREEN)
-    return surface
+    def __init__(self):
+        self.__surface = self.generate_bar_pair()
+        self.__x_pos = SCREEN_WIDTH
 
+    """
+    Returns the x position of the bar pair
+    """
+    def get_x(self):
+        return self.__x_pos
+
+    """
+    Returns the surface representation of the bar pair
+    """
+    def get_surface(self):
+        return self.__surface
+
+    """
+    Moves the bar pair to the left by SCROLL_RATE pixels
+    """
+    def scroll(self):
+        self.__x_pos -= SCROLL_RATE
+
+class BarList():
+    def __init__(self):
+        self.__list = []
+        self.__n_scrolls = 0
+
+    def to_tuple(self):
+        return tuple(self.__list)
+
+    def addNewBar(self):
+        self.__list.append(BarPair())
+
+    def scroll(self):
+        if len(self.__list) == 0:
+            self.addNewBar()
+        for pair in self.__list:
+            pair.scroll()
+        if self.__list[0].get_x() <= 0:
+            self.__list.pop()
+        if self.__n_scrolls % BAR_FREQUENCY == 0:
+            self.addNewBar()
+        self.__n_scrolls += 1
 
 #################################### Setup #####################################
 
@@ -59,7 +110,7 @@ pygame.display.set_caption("Flappy Bird")
 screen = pygame.display.set_mode(SCREEN_SIZE)
 
 background = pygame.Surface(SCREEN_SIZE).convert()   # background
-background.fill(BLUE)
+background.fill(BG_COLOUR)
 floor = pygame.Surface((SCREEN_WIDTH, GROUND_HEIGHT)).convert()
 floor.fill(GREY)
 background.blit(floor, (0, GROUND_LEVEL))
@@ -72,7 +123,7 @@ foreground.set_colorkey(BLACK)
 mainloop = True
 bird_y_velocity = GRAVITY
 clock = pygame.time.Clock()
-bar = generate_bar()
+bars = BarList()
 while mainloop:
     # handle events
     for event in pygame.event.get():
@@ -91,14 +142,15 @@ while mainloop:
         bird_y_velocity += GRAVITY
     bird_y += int(bird_y_velocity)
 
-    # redraw and update screen
+    # redraw foreground
     foreground.fill(BLACK)
+    bars.scroll()
     pygame.draw.circle(foreground,
                        BIRD_COLOUR,
                        (bird_x, bird_y),
                        BIRD_RADIUS)
-
-    foreground.blit(bar, (300, GROUND_LEVEL - bar.get_height()))
+    for pair in bars.to_tuple():
+        foreground.blit(pair.get_surface(), (pair.get_x(), 0))
 
     screen.blit(background, (0, 0))
     screen.blit(foreground, (0, 0))
